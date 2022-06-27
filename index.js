cbb4fff1d4055c9c7a971f0ed523b5a5d6c339bc
Lucas Lamin <lucaslamin15@gmail.com>
2022-06-26 23:25:25 -0300

    #Account Creator end

--- index.js
+++ index.js
@@ -31,4 +31,44 @@ function operation () {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por usar o nosso Banco! :)'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+    buildAccount()
+}
+
+function buildAccount() {
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta:'
+        }
+    ]).then(answer =>{
+        const accountName = answer['accountName']
+
+        console.info(answer['accountName'])
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(
+                chalk.bgRed.black('Esta conta já existe, escolha outro nome!'),
+            )
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`, 
+            '{"balance": 0}',
+            function (err) {
+                console.log(err)
+            },
+        )
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+    })
+    .catch(err => console.log(err))
+
 }
